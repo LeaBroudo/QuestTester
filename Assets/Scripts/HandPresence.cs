6ebf66fe32090d82f466526b7d6fdedfa57a98ca
Lea Broudo <leabroudo@leas-air.myfiosgateway.com>
2021-06-03 15:34:02 -0400

    Can switch between hands and controllers

--- Assets/Scripts/HandPresence.cs
+++ Assets/Scripts/HandPresence.cs
@@ -7,6 +7,8 @@ public class HandPresence : MonoBehaviour
     private OVRHand hand; 
     private SkinnedMeshRenderer handRenderer;
 
+    public OVRControllerHelper controllerHelper;
+
     private float lowConfidenceTime = 0;
     private float maxLowConfidenceTime = 1f;
 
@@ -20,8 +22,8 @@ public class HandPresence : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //if (hand.HandConfidence == OVRHand.TrackingConfidence.High) {
-        if (hand.IsTracked && hand.HandConfidence == OVRHand.TrackingConfidence.High) { //TODO: Check if controllers active
+        //if (hand.HandConfidence == OVRHand.TrackingConfidence.Low) { //TODO: transparency for this conditional
+        if (hand.IsTracked && !controllerHelper.activeController) {  
             bool isIndexFingerPinching = hand.GetFingerIsPinching(OVRHand.HandFinger.Index);
             float indexFingerPinchStrength = hand.GetFingerPinchStrength(OVRHand.HandFinger.Index);
             OVRHand.TrackingConfidence confidence = hand.GetFingerConfidence(OVRHand.HandFinger.Index);
